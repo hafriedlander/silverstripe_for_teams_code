5b4256271c0bd09fc3360b679778fadb8495c76f
Hamish Friedlander <hamish@silverstripe.com>
2016-02-18 15:33:51 +0000

    Fix Todo and TodoSet so you cant associate them with each other if the OwnerIDs mismatch

--- mysite/code/models/Todo.php
+++ mysite/code/models/Todo.php
@@ -10,4 +10,15 @@ class Todo extends DataObject {
 		'Set' => 'TodoSet',
 		'Owner' => 'Member'
 	];
+
+	function getCMSFields() {
+		// Get the scaffolded fields
+		$fields = parent::getCMSFields();
+
+		$field = $fields->fieldByName('Root.Main.SetID');
+		$field->setSource(TodoSet::get()->filter('OwnerID', $this->OwnerID)->map());
+
+		return $fields;
+	}
+
 }
--- mysite/code/models/TodoSet.php
+++ mysite/code/models/TodoSet.php
@@ -12,4 +12,24 @@ class TodoSet extends DataObject {
 	private static $has_one = [
 		'Owner' => 'Member'
 	];
+
+	function getCMSFields() {
+		// Get the scaffolded fields
+		$fields = parent::getCMSFields();
+		$fields->removeFieldFromTab('Root', 'Todos');
+
+		// Create a GridField configuration based off an existing template
+		$config = GridFieldConfig_RelationEditor::create();
+
+		// Get the "Add Existing Autocompleter" component
+		$addExisting = $config->getComponentsByType('GridFieldAddExistingAutocompleter')->first();
+		// Adjust the search list to only search Todos owned by the same owner as this set
+		$addExisting->setSearchList(Todo::get()->filter('OwnerID', $this->OwnerID));
+
+		// Create a new GridField based on that configuration for the Todos relationship
+		$grid = GridField::create('Todos', 'Todos', $this->Todos(), $config);
+		$fields->addFieldToTab('Root.Main', $grid);
+
+		return $fields;
+	}
 }
